df58c28917857453fd289e024c4506638490de60
Muhammed Ali <muhamzyali@gmail.com>
2023-01-14 16:21:39 +0100

    final

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from app.models import *
+from django.db.models.functions import TruncDay
+from django.db.models import Count
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+
 
 # Register your models here.
+
+class WriterAdmin(admin.ModelAdmin):
+
+    ## change_list.html
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new writer per day
+        chart_data = (
+    	    Writer.objects.annotate(date=TruncDay("createdDate"))
+            .values("date")
+            .annotate(y=Count("id"))
+            .order_by("-date")
+        )
+
+        # Serialize and attach the chart data to the template context
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        print("Json %s"%as_json)
+        extra_context = extra_context or {"chart_data": as_json}
+        # Call the superclass changelist_view to render the page
+        
+        return super().changelist_view(request, extra_context=extra_context)
+
+
+admin.site.register(Writer, WriterAdmin)
+admin.site.register(Article)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Writer(models.Model):
+    name = models.CharField(max_length=200)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return u'%s' % (self.name)
+
+
+class Article(models.Model):
+    text_title = models.CharField(max_length=50, null=True, blank=True)
+    text = models.TextField(max_length=200, null=True, blank=True)
+    refWriter = models.ForeignKey(Writer, on_delete=models.CASCADE)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return u'[%s] : %s' % (self.refWriter,self.text_title)
